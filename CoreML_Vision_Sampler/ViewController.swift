10a4b0ca62c389f649294ea9c3f9145ebe0fc174
natmark <natmark0918@gmail.com>
2018-06-30 21:51:57 +0900

    :+1: Update ViewController

--- CoreML_Vision_Sampler/ViewController.swift
+++ CoreML_Vision_Sampler/ViewController.swift
@@ -7,29 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let captureSession = CaptureSession(delegate: self, previewView: self.previewView)
+        let request = MobileNet()
 
-        // Do any additional setup after loading the view.
+        indicatorView.startAnimating()
+        MLModelCompiler.request(request) { result in
+            switch result {
+            case .left(let model):
+                CaptureAnalyzer.mlModel = try? VNCoreMLModel(for: model)
+
+                self.indicatorView.stopAnimating()
+                self.indicatorView.isHidden = true
+                captureSession.startRunning()
+            case .right(_): break
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CaptureAnalyzer.analyze(pixelBuffer: pixelBuffer) { result in
+            switch result {
+            case .left(let identifier):
+                self.label.text = identifier
+            case .right(_): break
+            }
+        }
+    }
+}
+
